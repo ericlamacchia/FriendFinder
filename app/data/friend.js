59ba5192d4b9bb681d2e38858709fc92b437367b
Eric LaMacchia <ericlamacchia1@icloud.com>
2019-10-15 16:58:12 -0400

    Made the friend data

---
+++ app/data/friend.js
@@ -0,0 +1,46 @@
+var friends = [
+    {
+      name: "Steve Mcqueen",
+      photo:
+        "https://m.media-amazon.com/images/M/MV5BMTQ3Mjk2MTU1MV5BMl5BanBnXkFtZTcwMTA5MTkzNA@@._V1_.jpg",
+      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+      name: "Will Ferrell",
+      photo:
+        "https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzc3ODM1MDIy/will-ferrell-9542601-1-402.jpg",
+      scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+      name: "Steve Jobs",
+      photo:
+        "https://cdn.cultofmac.com/wp-content/uploads/2017/10/DLYBl8rW4AEcgww.jpg-large-780x520.jpg",
+      scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+      name: "Idris Elba",
+      photo:
+        "https://cdn.britannica.com/41/188641-050-AB88F70B/Idris-Elba-British.jpg",
+      scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+      name: "Michael Scott",
+      photo:
+        "https://i.redd.it/7ib98txs7yi21.jpg",
+      scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+      name: "Pam Beesly",
+      photo:
+        "https://pics.me.me/pam-beesly-villain-played-psycho-sexual-mind-games-w-jim-while-33540294.png",
+      scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+    },
+    {
+      name: "Jennifer Lawrence",
+      photo:
+        "https://nyppagesix.files.wordpress.com/2019/09/jennifer-lawrence.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+      scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    }
+  ];
+  module.exports = friends;
+  
